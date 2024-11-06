a047e2c87be0b2728b327b899c1563a35d5df0c5
KEELANMURPHY-STUDENT <g00435965@atu.ie>
2024-11-06 16:37:40 +0000

    asking customer for info such as Id, name, email and phone number.

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,6 +1,29 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.scanner;
-public class Customer {
 
-        }
+import java.util.Scanner;
+
+public class Customer {
+    public static void main(String[] args) {
+
+        Scanner customerInput = new Scanner(System.in);
+        //Gather details of first customer
+        System.out.println("Please enter the customers unique ID");
+        String studentId = customerInput.nextLine();
+
+        System.out.println("Please enter the customers name");
+        String studentName = customerInput.nextLine();
+
+        System.out.println("Please enter the customers email address");
+        String studentEmail = customerInput.nextLine();
+
+        System.out.println("Please enter the customers phone number");
+        String studentNumber = customerInput.nextLine();
+
+        Customer firstCustomer = new Customer();
+        firstCustomer.setIdCustomer(customerId);
+        firstCustomer.setNameCustomer(customerName);
+        firstCustomer.setEmailCustomer(customerEmail);
+        firstCustomer.setNumberCustomer(customerNumber);
+
+    }
+}
+
---
+++ src/main/java/CustomerOther.java
@@ -0,0 +1,47 @@
+
+public class CustomerOther {
+
+    private String idCustomer;
+    private String nameCustomer;
+    private String emailCustomer;
+    private String numberCustomer;
+
+    public CustomerOther() {
+        idCustomer = "";
+        nameCustomer = "";
+        emailCustomer = "";
+        numberCustomer = "";
+    }
+
+    public String getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setIdCustomer(String idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    public String getNameCustomer() {
+        return nameCustomer;
+    }
+
+    public void setNameCustomer(String nameCustomer) {
+        this.nameCustomer = nameCustomer;
+    }
+
+    public String getEmailCustomer() {
+        return emailCustomer;
+    }
+
+    public void setEmailCustomer(String emailCustomer) {
+        this.emailCustomer = emailCustomer;
+    }
+
+    public String getNumberCustomer() {
+        return numberCustomer;
+    }
+
+    public void setNumberCustomer(String numberCustomer) {
+        this.numberCustomer = numberCustomer;
+    }
+}
